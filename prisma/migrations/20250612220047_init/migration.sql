6ea00fb16e035e0710930a61937526a0b330ca9a
Tailhan Celine <tailhan.celine@hotmail.fr>
2025-06-16 22:59:56 +0200

    Generate add_prestation migration

--- prisma/migrations/20250612220047_init/migration.sql
+++ prisma/migrations/20250612220047_init/migration.sql
@@ -255,22 +255,3 @@ ALTER TABLE "_DomainToTraining" ADD CONSTRAINT "_DomainToTraining_A_fkey" FOREIG
 
 -- AddForeignKey
 ALTER TABLE "_DomainToTraining" ADD CONSTRAINT "_DomainToTraining_B_fkey" FOREIGN KEY ("B") REFERENCES "Training"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- CreateTable for Prestation
-CREATE TABLE "Prestation" (
-    "id" SERIAL NOT NULL,
-    "type" TEXT NOT NULL,
-    "tech" TEXT NOT NULL,
-    "level" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
-    CONSTRAINT "Prestation_pkey" PRIMARY KEY ("id")
-);
-
--- CreateIndex for Prestation
-CREATE INDEX "Prestation_userId_index" ON "Prestation"("userId");
-
--- AddForeignKey for Prestation
-ALTER TABLE "Prestation"
-  ADD CONSTRAINT "Prestation_userId_fkey"
-  FOREIGN KEY ("userId") REFERENCES "User"("id")
-  ON DELETE RESTRICT ON UPDATE CASCADE;

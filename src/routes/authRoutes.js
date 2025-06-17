b303e372a24ef8d2be22413f850ecd050f63a520
Tailhan Celine <tailhan.celine@hotmail.fr>
2025-06-17 10:55:14 +0200

    Expose route GET /confirm-email

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signup, login, me } = require('../controllers/authController');
+const { signup, login, me, confirmEmail} = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware'); 
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.get('/confirm-email', confirmEmail)
 router.post('/login', login);
 router.get('/me', authMiddleware, me);
 

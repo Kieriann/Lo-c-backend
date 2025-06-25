4de35234ce5e7822f3b984b0dc9881afaefdaa29
Tailhan Celine <tailhan.celine@hotmail.fr>
2025-06-25 17:05:28 +0200

    05

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -15,7 +15,7 @@ router.post(
   '/profil',
   upload.fields([
     { name: 'photo' },
-    { name: 'CV' }
+    { name: 'cv' }
   ]),
   async (req, res) => {
     try {
@@ -68,14 +68,14 @@ router.post(
       }
 
       if (req.body.removeCV === 'true') {
-        const cvDoc = await prisma.document.findFirst({ where: { userId, type: 'CV' } });
+        const cvDoc = await prisma.document.findFirst({ where: { userId, type: 'cv' } });
         if (cvDoc) {
           await prisma.document.delete({ where: { id: cvDoc.id } });
         }
       }
 
       const photoFile = req.files?.photo?.[0];
-      const cvFile = req.files?.CV?.[0];
+      const cvFile = req.files?.cv?.[0];
       console.log('cv FILE:', cvFile);
       console.log('Taille buffer cv:', cvFile?.buffer?.length);
 

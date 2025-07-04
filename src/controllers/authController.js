03d2b6fef9c9a290530df532c89952fade6d6f16
Tailhan Celine <tailhan.celine@hotmail.fr>
2025-07-04 10:47:06 +0200

    finalisation vouvoiement mail, accessibilité

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -46,8 +46,8 @@ async function signup(req, res) {
     to:      email,
     from:    'no-reply@freesbiz.fr',
     subject: 'Merci pour votre inscription – dernière étape',
-    text: `Bonjour ${prenom},\n\nMerci pour votre inscription sur Free’s Biz.\n\nPour finaliser votre compte, cliquez sur le lien suivant :\n${confirmUrl}\n\nSi vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer ce message.`,
-    html: `<p>Bonjour ${prenom},</p><p>Merci pour votre inscription sur <strong>Free’s Biz</strong>.</p><p>Pour finaliser votre compte, cliquez <a href="${confirmUrl}">ici</a>.</p><p>Si vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer ce message.</p>`,
+    text: `Bonjour,\n\nMerci pour votre inscription sur Free’s Biz.\n\nPour finaliser votre compte, cliquez sur le lien suivant :\n${confirmUrl}\n\nSi vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer ce message.`,
+    html: `<p>Bonjour,</p><p>Merci pour votre inscription sur <strong>Free’s Biz</strong>.</p><p>Pour finaliser votre compte, cliquez <a href="${confirmUrl}">ici</a>.</p><p>Si vous n’êtes pas à l’origine de cette demande, vous pouvez ignorer ce message.</p>`,
   })
 
   return res
